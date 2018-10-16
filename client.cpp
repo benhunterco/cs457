1cf7e78f1dfb0f6e97b44792f6dae56302db6459
Ben Newell <benhunterco@gmail.com>
2018-10-16 14:40:51 -0600

    adds mutex to client socket send

--- client.cpp
+++ client.cpp
@@ -4,23 +4,24 @@
 #include <unistd.h>
 #include <string>
 #include <iostream>
+#include "tcpUserSocket.h"
 #include "tcpClientSocket.h"
 
-int main (int argc, char **argv)
-{
+//Values passed into client from command line call.
+//Not read from config file, that will be implemented later.
+std::string hostname = "";
+std::string username = "";
+int serverport = 0;
+std::string configFile = "";
+std::string testFile = "";
+std::string logFile = "";
 
-    //Values passed into client from command line call. 
-    //Not read from config file, that will be implemented later. 
-    std::string hostname = "";
-    std::string username = "";
-    int serverport = 0;
-    std::string configFile = "";
-    std::string testFile = "";
-    std::string logFile = "";
+int main(int argc, char **argv)
+{
 
     opterr = 0;
     char c = ' ';
-    while ((c = getopt (argc, argv, "h:u:p:c:t:L:")) != -1)
+    while ((c = getopt(argc, argv, "h:u:p:c:t:L:")) != -1)
         switch (c)
         {
         case 'h':
@@ -31,7 +32,8 @@ int main (int argc, char **argv)
             break;
         case 'p':
             serverport = atoi(optarg);
-            if(serverport == 0){
+            if (serverport == 0)
+            {
                 std::cerr << "Incorrect port number. Please enter an integer\n";
                 return 1;
             }
@@ -50,21 +52,23 @@ int main (int argc, char **argv)
             std::cerr << "Incorrect usage. Options are -h hostname -u username -p portnumber -c configfile -t testfile -L logfile\n";
             return 1;
         default:
-            abort ();
+            abort();
         }
 
-    //for now don't do anything, but presumably we could call stuff later. 
-    std::cout << "Hostname: " << hostname << " Username: "<< username << " ServerPort: " << serverport << " configfile: "  
-        << configFile << " TestFile: "<< testFile << " LogFile: " << logFile << "\n";
-
-    //Make the socket.
-    cs457::tcpClientSocket client(2000, std::string("127.0.0.1"));
-    std::string message;
+    //for now don't do anything, but presumably we could call stuff later.
+    std::cout << "Hostname: " << hostname << " Username: " << username << " ServerPort: " << serverport << " configfile: "
+              << configFile << " TestFile: " << testFile << " LogFile: " << logFile << "\n";
+    
+    //create the socket
+    cs457::tcpClientSocket client(serverport, hostname);
     while(true)
     {
-        std::cout << "Input you message: ";
-        std::cin >> message;
-        client.sendString(message);
+        std::cout << "input your message: ";
+        std::string input;
+        getline(std::cin, input);
+        input += "\n";
+        client.sendString(input, true);
     }
+        
     return 0;
 }
