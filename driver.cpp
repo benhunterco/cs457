519b18e26c11c86752fba2f9e6196b4e922a8b76
benhunterco <benhunterco@gmail.com>
2018-10-18 12:49:34 -0600

    added administration thread to server

--- driver.cpp
+++ driver.cpp
@@ -3,72 +3,83 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <iostream>
-#include <string> 
-#include <tuple> 
-#include <thread> 
-#include <vector> 
-#include <memory> 
+#include <string>
+#include <tuple>
+#include <thread>
+#include <vector>
+#include <memory>
 #include "tcpUserSocket.h"
 #include "tcpServerSocket.h"
 
 using namespace std;
 
-bool ready = true; 
+bool ready = true;
 
-int cclient(shared_ptr<cs457::tcpUserSocket> clientSocket,int id)
+int cclient(shared_ptr<cs457::tcpUserSocket> clientSocket, int id)
 {
 
     cout << "Waiting for message from Client Thread" << id << std::endl;
     string msg;
     ssize_t val;
-    bool cont =true ;  
-    while (cont) 
+    bool cont = true;
+    while (cont)
     {
-        tie(msg,val) = clientSocket.get()->recvString();
+        tie(msg, val) = clientSocket.get()->recvString();
         //if message is blank, continue.
         //Might solve ^C thing.
-        if (msg.substr(0,4) == "EXIT")
-            cont = false; 
-       
+        if (msg.substr(0, 4) == "EXIT")
+            cont = false;
+
         cout << "[SERVER] The client is sending message " << msg << " -- With value return = " << val << endl;
-        string s =  "[SERVER REPLY] The client is sending message:" + msg  + "\n"; 
-        thread childT1(&cs457::tcpUserSocket::sendString,clientSocket.get(),s,true);
+        string s = "[SERVER REPLY] The client is sending message:" + msg + "\n";
+        thread childT1(&cs457::tcpUserSocket::sendString, clientSocket.get(), s, true);
         //thread childT2(&cs457::tcpUserSocket::sendString,clientSocket.get(),msg,true);
         //thread childT3(&cs457::tcpUserSocket::sendString,clientSocket.get(),"\n",true);
-        
+
         childT1.join();
-        //childT2.join(); 
-        //childT3.join(); 
-        //clientSocket.get()->sendString(msg); 
-        //clientSocket.get()->sendString("\n"); 
-         if (msg.substr(0,6) == "SERVER")
+        //childT2.join();
+        //childT3.join();
+        //clientSocket.get()->sendString(msg);
+        //clientSocket.get()->sendString("\n");
+        if (msg.substr(0, 6) == "SERVER")
         {
-            thread childTExit(&cs457::tcpUserSocket::sendString,clientSocket.get(),"GOODBYE EVERYONE",false);
-            thread childTExit2(&cs457::tcpUserSocket::sendString,clientSocket.get(),"\n",false);
-            ready = false;   
-            cont = false;   
-            childTExit.join(); 
+            thread childTExit(&cs457::tcpUserSocket::sendString, clientSocket.get(), "GOODBYE EVERYONE", false);
+            thread childTExit2(&cs457::tcpUserSocket::sendString, clientSocket.get(), "\n", false);
+            ready = false;
+            cont = false;
+            childTExit.join();
             childTExit2.join();
         }
         else
         {
-            cout << "waiting for another message" << endl; 
+            cout << "waiting for another message" << endl;
         }
     }
 
-    clientSocket.get()->sendString("goodbye"); 
-    
-    clientSocket.get()->closeSocket(); 
-    return 1; 
+    clientSocket.get()->sendString("goodbye");
+
+    clientSocket.get()->closeSocket();
+    return 1;
+}
+
+void adminCommands()
+{
+    string command;
+    while (command.find("STOP") == string::npos)
+    {
+        cout << "[SERVER]>";
+        getline(cin, command);
+        cout << command << endl;
+    }
 }
 
 int port = 2000;
 string configFile = "chatserver.conf";
 string db = "db/";
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
-     opterr = 0;
+    opterr = 0;
     char c = ' ';
     while ((c = getopt(argc, argv, "p:c:d:")) != -1)
         switch (c)
@@ -92,39 +103,37 @@ int main(int argc, char * argv[])
         default:
             abort();
         }
-    cout << "Initializing Socket on port" << port << std::endl; 
+
+    cout << "Initializing Socket on port " << port << std::endl;
     cs457::tcpServerSocket mysocket(port);
-    cout << "Binding Socket" << std::endl; 
-    mysocket.bindSocket(); 
-    cout << "Listening Socket" << std::endl; 
-    mysocket.listenSocket(); 
+    cout << "Binding Socket" << std::endl;
+    mysocket.bindSocket();
+    cout << "Listening Socket" << std::endl;
+    mysocket.listenSocket();
     cout << "Waiting to Accept Socket" << std::endl;
-    int id = 0; 
-    vector<unique_ptr<thread>> threadList; 
-  
+    int id = 0;
+    vector<unique_ptr<thread>> threadList;
+    cout << "Starting administration thread here??? \n";
+    thread adminThread(adminCommands);
     while (ready)
-    { 
+    {
         shared_ptr<cs457::tcpUserSocket> clientSocket;
-        int val; 
-        tie(clientSocket,val) = mysocket.acceptSocket();
-        cout << "value for accept is " << val << std::endl; 
-        cout << "Socket Accepted" << std::endl; 
-        unique_ptr<thread> t = make_unique<thread>(cclient,clientSocket,id); 
-        threadList.push_back(std::move(t)); 
-        
-        id++; //not the best way to go about it. 
-       // threadList.push_back(t); 
-       
-    
-        
+        int val;
+        tie(clientSocket, val) = mysocket.acceptSocket();
+        cout << "value for accept is " << val << std::endl;
+        cout << "Socket Accepted" << std::endl;
+        unique_ptr<thread> t = make_unique<thread>(cclient, clientSocket, id);
+        threadList.push_back(std::move(t));
+
+        id++; //not the best way to go about it.
+              // threadList.push_back(t);
     }
 
-    for (auto& t: threadList)
+    for (auto &t : threadList)
     {
         t.get()->join();
     }
-    
-        
-    cout << "Server is shutting down after one client" << endl; 
-    return 0; 
+    adminThread.join();
+    cout << "Server is shutting down after one client" << endl;
+    return 0;
 }
