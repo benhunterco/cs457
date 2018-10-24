658f0406c4d52776a605d46735fc85b7dd1a0d6b
Ben Newell <benhunterco@gmail.com>
2018-10-23 20:22:20 -0600

    adds server classes

---
+++ server.cpp
@@ -0,0 +1,33 @@
+#include "server.h"
+
+bool cs457::server::addUser(cs457::user newUser)
+{
+    
+    if (!userMap.count(newUser.getName()))
+    {
+        userMap.insert(std::make_pair(newUser.getName(), newUser));
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+    
+   return false;
+}
+
+bool cs457::server::addChannel(cs457::user requestingUser, std::string channelName)
+{
+    for (cs457::channel c : channels)
+    {
+        if (c.name == channelName)
+        {
+            return false;
+        }
+    }
+
+    cs457::channel newChannel;
+    newChannel.name = channelName;
+    newChannel.members.push_back(requestingUser); //can use first member as op???
+    return true;
+}
